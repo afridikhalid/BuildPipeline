dc4e873ac041dbdd356656c178e11dc266a9c080
Khalid Afridi <khalidafridi@Khalids-MacBook-Pro.local>
2017-05-05 14:19:26 +0200

    first commit

---
+++ BuildPipeline/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BuildPipeline
+//
+//  Created by Khalid Afridi on 2017-05-05.
+//  Copyright © 2017 Jayway. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
